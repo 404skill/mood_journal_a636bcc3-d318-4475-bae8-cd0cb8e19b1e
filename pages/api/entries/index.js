f1fce735cf67f50ec7a02a991d852266805aedca
Barak Uziel <barakuziel1@gmail.com>
2025-06-26 00:19:39 +0300

    add some validation

--- pages/api/entries/index.js
+++ pages/api/entries/index.js
@@ -1,7 +1,23 @@
-import { withHandler } from '../../../lib/withHandler';
-import { journalEntryService as service } from '../../../services/JournalEntryService';
+import { withHandler } from "../../../lib/withHandler";
+import { journalEntryService as service } from "../../../services/JournalEntryService";
+import { ValidationError } from "../../../services/JournalEntryService";
 
 async function listHandler(req, res) {
+  const { startDate, endDate, moods } = req.query;
+
+  if (startDate) {
+    const start = new Date(startDate);
+    if (isNaN(start)) {
+      throw new ValidationError("Invalid date format");
+    }
+  }
+  if (endDate) {
+    const end = new Date(endDate);
+    if (isNaN(end)) {
+      throw new ValidationError("Invalid date format");
+    }
+  }
+
   const entries = await service.list(req.query);
   res.status(200).json(entries);
 }
@@ -11,9 +27,14 @@ async function postHandler(req, res) {
   res.status(201).json(newEntry);
 }
 
-export default withHandler(async (req, res) => {
-  switch (req.method) {
-    case 'GET': return listHandler(req, res);
-    case 'POST': return postHandler(req, res);
-  }
-}, ['GET','POST']);
+export default withHandler(
+  async (req, res) => {
+    switch (req.method) {
+      case "GET":
+        return listHandler(req, res);
+      case "POST":
+        return postHandler(req, res);
+    }
+  },
+  ["GET", "POST"]
+);
